0efc57bf3c17a9346455b499c26ac5f8bf6a3284
aleze89 <68794504+aleze89@users.noreply.github.com>
2020-10-07 21:35:46 -0300

    modificacion en boton color,ahora cambia de un estado al otro

--- js/main.js
+++ js/main.js
@@ -10,23 +10,56 @@ $(document).ready(function(){
         
     
 
-    
+var $botones = $('#color');
+
+$botones.click(evento => {
+  var $elemento = $(evento.target);
+   
+  if ($elemento.hasClass('black')) {
+    $elemento.removeClass('black');
+    $( "body" ).css('background', 'linear-gradient(rgba(238, 156, 167,0.3),rgba(255, 179, 187,0.3),rgba(255, 221, 225,0.3))');
+    //$( "body" ).css( "background-color", "black" );
+  }
+  else {
+    $elemento.addClass('black');
+    //$( "body" ).css('background', 'linear-gradient(rgba(238, 156, 167,0.3),rgba(255, 179, 187,0.3),rgba(255, 221, 225,0.3))'); 
+    $( "body" ).css( "background-color", "black" );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     //función que se ejecutará al presionar el botón #color
     $("#color").on("click",function(){
         
         //cambio color del body
-        // $( "body" ).css( "background-color", "red" );
+         $( "body" ).css( "background-color", "black" );
         
         //cambiamos texto según lo que corresponde
-        var colorActual = $(this).filter($("#color"));
+        /*var colorActual = $(this).filter($());
         if(colorActual == true){
             $( "body" ).css( "background-color", "red" );
-        } /* else{
+        }  else{
             $( "body" ).css("background: linear-gradient(rgba(238, 156, 167,0.3),rgba(255, 179, 187,0.3),rgba(255, 221, 225,0.3))"); 
-        }   */            
+        }               
 
         
-    });
+    });*/
 
 
         
